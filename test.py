57720c5163eb0a8d51f009fcc306334ec3b2bf76
Rabia Cansel UĞUR <r4bi4cnsl@gmail.com>
2025-06-14 15:04:09 +0300

    Update test.py

--- test.py
+++ test.py
@@ -5,10 +5,10 @@ from PIL import Image
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 G = Generator().to(device)
-G.load_state_dict(torch.load(r"C:\Users\Casper\OneDrive\Masaüstü\Üniversiteye Dair Her Şey\3.Sınıf\bahar dönemi\Görüntü İşleme\Projeler\Renklendirme\saved_models2\generator_epoch238.pth", map_location=device))
+G.load_state_dict(torch.load(r", map_location=device))
 G.eval()
 
-img = Image.open(r"C:\Users\Casper\OneDrive\Masaüstü\flower.jpg").convert("L")
+img = Image.open(r).convert("L")
 
 transform = transforms.Compose([
     transforms.Resize((1024, 1024)),
@@ -21,8 +21,6 @@ with torch.no_grad():
     y = G(x).squeeze().cpu() * 0.5 + 0.5
 
 out = transforms.ToPILImage()(y.clamp(0, 1))
-out.save(r"C:\Users\Casper\OneDrive\Masaüstü\Üniversiteye Dair Her Şey\3.Sınıf\bahar dönemi\Görüntü İşleme\Projeler\Renklendirme\results2\generated_238_f4.jpg")
-
 
 
 
